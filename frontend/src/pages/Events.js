c27feaee9cb89bcd73e2a4167a838631c3f17253
justicepelteir <justice.douglas@hey.com>
2023-04-09 20:12:32 -0700

    add error management responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
-
 function EventsPage() {
   const data = useLoaderData();
 
@@ -17,11 +16,13 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events123");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return {isError: true, message: 'Could not fetch events.'};
-    throw { message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
