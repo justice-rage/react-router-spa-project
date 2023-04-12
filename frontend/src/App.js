5d1169f93ba4cb1128a807e78dcc98e70b26fd4f
justicepelteir <justice.douglas@hey.com>
2023-04-11 18:15:27 -0700

    add action functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import EventDetailPage, {
 } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader, loader } from "./pages/Events";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ]
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
